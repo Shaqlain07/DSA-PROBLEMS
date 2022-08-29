36904ccc3e76ba3bd70b704a3001e06a5272378e
Shaqlain07 <104688734+Shaqlain07@users.noreply.github.com>
2022-08-29 23:36:56 +0530

    Create MiddleElementInList.cpp

---
+++ LinkedList/MiddleElementInList.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        // Your code here
+        if(head==NULL) {
+            return head->data;
+        }
+        Node* temp=head;
+        int l=0;
+        while(temp) { //find length of linked list
+            l++;
+            temp=temp->next;
+        }
+        int middle=(l/2)+1;
+        Node* dummy=head;
+        int count=1;
+        while(count!=middle) {
+            count++;
+            dummy=dummy->next;
+        }
+        return dummy->data;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        cout << ob.getMiddle(head) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
