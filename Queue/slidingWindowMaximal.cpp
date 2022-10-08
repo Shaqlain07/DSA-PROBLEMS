9dd99694052b765811d004c74bb0aa42ed20365d
Shaqlain07 <104688734+Shaqlain07@users.noreply.github.com>
2022-10-08 23:34:02 +0530

    Create slidingWindowMaximal.cpp

---
+++ Queue/slidingWindowMaximal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> dq;
+        for(int i=0;i<nums.size();i++){
+            if(!dq.empty() && dq.front()==i-k){//front is out of window
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[dq.back()]<nums[i]){//smaller element i dq will be                                                                  //removed
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i>=k-1){
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+        return ans;
+    }
+};
