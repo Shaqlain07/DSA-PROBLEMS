c0e475478ea299c6587ecf087ec86a7843f31d47
Shaqlain07 <104688734+Shaqlain07@users.noreply.github.com>
2022-11-24 20:00:45 +0530

    Create convertSortedArrayToBST.cpp

---
+++ Trees/convertSortedArrayToBST.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* helper(vector<int> nums,int left,int right) {
+        if(left>right) {
+            return NULL;
+        }
+        int mid=left+(right-left)/2;
+        TreeNode* root=new TreeNode(nums[mid]);//root created
+        root->left=helper(nums,left,mid-1);
+        root->right=helper(nums,mid+1,right);
+        
+        return root;
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if(nums.size()==0) {
+            return NULL;
+        }
+        return helper(nums,0,nums.size()-1);
+    }
+};
