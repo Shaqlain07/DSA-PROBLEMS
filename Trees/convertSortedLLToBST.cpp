f246a52acab21ed3a2abf540cca922b116dc6cd3
Shaqlain07 <104688734+Shaqlain07@users.noreply.github.com>
2022-11-24 22:57:59 +0530

    Use Binary search using 2 pointer.

---
+++ Trees/convertSortedLLToBST.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* helper(ListNode* head,ListNode* end) {
+        if(head==end) return NULL;
+        //finding middle node
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=end && fast->next!=end) {
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        //defining root
+        TreeNode* root=new TreeNode(slow->val);
+        root->left=helper(head,slow);
+        root->right=helper(slow->next,end);
+        
+        return root;
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(head==NULL) return NULL;
+        return helper(head,NULL);
+        
+    }
+};
