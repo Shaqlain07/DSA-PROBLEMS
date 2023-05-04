b9f3c466670688937b667e87e1787762b175d504
Shaqlain07 <104688734+Shaqlain07@users.noreply.github.com>
2023-05-04 11:00:47 +0530

    Create uniqueBST(96).cpp

---
+++ Dynamic Programmic/uniqueBST(96).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ///recursion+Memoization(top down)
+    int solve(int n,vector<int> &dp) {
+        //base case
+        if(n<=1) return 1;
+
+        if(dp[n]!=-1) return dp[n];
+        int ans=0;
+        for(int i=1;i<=n;i++) {
+            ans+=solve(i-1,dp)*solve(n-i,dp);
+        }
+        return dp[n]=ans;
+    }
+    int numTrees(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
