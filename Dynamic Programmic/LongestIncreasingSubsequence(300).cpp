8649299fed04d79e5300a81ea60ab8b05e1488bb
Shaqlain07 <104688734+Shaqlain07@users.noreply.github.com>
2023-05-08 17:24:55 +0530

    Create LongestIncreasingSubsequence(300).cpp

---
+++ Dynamic Programmic/LongestIncreasingSubsequence(300).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    //T.C-(O(N*logN))
+    //S.C-O(N)
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int len=1;
+        for(int i=1;i<n;i++) {
+            if(nums[i]>temp.back()) {
+                temp.push_back(nums[i]);
+                len++;
+            }
+            else {
+                int idx=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[idx]=nums[i];
+            }
+        }
+        return len;
+    }
+};
