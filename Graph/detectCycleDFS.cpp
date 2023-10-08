101b92f53c0e66964977e8f14c2d0a0d52889eaf
Shaqlain07 <104688734+Shaqlain07@users.noreply.github.com>
2023-10-08 17:52:16 +0530

    Create detectCycleDFS.cpp

---
+++ Graph/detectCycleDFS.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool dfs(int src,vector<int> adj[],vector<int> &vis,int parent) {
+        
+        vis[src]=1;
+        for(auto it:adj[src]) {
+            if(!vis[it]) {
+                if(!dfs(it,adj,vis,src)){
+                 return false;   
+                }
+            }
+            else if(parent!=it) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        vector<int> vis(V,0);
+        for(int i=0;i<V;i++) {
+            if(!vis[i]) {
+                if(!dfs(i,adj,vis,-1)) {
+                   return true; 
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
